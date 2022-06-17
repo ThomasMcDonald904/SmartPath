9fed8d0b2a9d354e5725365be8074e07a1240a5e
ThomasMcDonald904 <naynaytom3000@gmail.com>
2022-06-17 16:26:11 -0400

    Fixed WIP

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template
 from requests import request
 
-import routing_management
+from routing_management import *
 
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates/')
+
 
 @app.route("/")
 def home():
+    print(get_route(address_to_lonlat("904 rue Arthur-McNicoll", "Shawinigan"), address_to_lonlat("1029 rue Notre-Dame", "Champlain")))
     return render_template("home.html")
 
 @app.route("/route_submit", methods=["GET", "POST"])
