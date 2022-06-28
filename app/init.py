855dfe846682f8e89bebbb6eb7cd9e1e6816a20a
ThomasMcDonald904 <naynaytom3000@gmail.com>
2022-06-27 23:36:44 -0400

    Last ditch attemp for external import

--- app/init.py
+++
@@ -1,3 +0,0 @@
-from flask import Flask, render_template, request
-import routing_management
-from flask_cors import CORS, cross_origin
--- app/main.py
+++ app/main.py
@@ -1,4 +1,9 @@
-from init import *
+from flask import Flask, render_template, request
+from flask_cors import CORS, cross_origin
+import json
+import requests
+import urllib.parse
+
 
 app = Flask(__name__, template_folder='templates/')
 CORS(app)
@@ -27,15 +32,61 @@ def routing_form():
 
     waypoint_list = []
     for pair in address_city_pairs:
-        waypoint_list.append(routing_management.address_to_lonlat(pair[0], pair[1]))
+        waypoint_list.append(address_to_lonlat(pair[0], pair[1]))
 
-    json_data = routing_management.generate_waypoints_json(*waypoint_list)
+    json_data = generate_waypoints_json(*waypoint_list)
 
     with open('tempdata/json_waypoints.json', 'w') as json_file:
         json_file.write(json_data)
 
     with open("tempdata/json_waypoints.json", "r") as json_file:
-        route = routing_management.get_route(json_file)
+        route = get_route(json_file)
 
     return route
-    
+
+
+# ----------------------------
+# Libraries
+# ----------------------------
+
+tomtom_api_key = "NGGuMr6A0UbNGS8ZB1egkfviHxZ4ki2W"
+
+
+
+def address_to_lonlat(_address: str, _city: str, api_key=tomtom_api_key):
+    url_encoded_address = urllib.parse.quote(_address)
+    api_call_geocode = f"https://api.tomtom.com/search/2/geocode/{url_encoded_address}.json?storeResult=false&countrySet=Canada&view=Unified&key={api_key}"
+
+    response = requests.request("GET", api_call_geocode)
+    data = response.json()
+
+    for i in data["results"]:
+        if i["type"] == "Point Address" and i["address"]["municipality"] == _city:
+            user_address_lon_lat = i["position"]
+
+    # formatted_lon_lat = urllib.parse.quote(f"{user_address_lon_lat['lat']},{user_address_lon_lat['lon']}")
+    # return user_address_lon_lat['lat'], user_address_lon_lat['lon']
+    return {"point": {"latitude": user_address_lon_lat["lat"], "longitude": user_address_lon_lat["lon"]}}
+
+
+def lon_lat_to_address(lon_lat_dict, api_key=tomtom_api_key):
+    lon_lat_pair = ",".join([str(lon_lat_dict["latitude"]), str(lon_lat_dict["longitude"])])
+    lon_lat_pair_encoded = urllib.parse.quote(lon_lat_pair)
+    api_call_reverse_geocode = f"https://api.tomtom.com/search/2/reverseGeocode/crossStreet/{lon_lat_pair_encoded}.json?returnSpeedLimit=false&radius=10000&returnRoadUse=false&callback=cb&allowFreeformNewLine=false&returnMatchType=false&view=Unified&key={api_key}"
+    response = requests.request("GET", api_call_reverse_geocode)
+    data = response.json()
+    return data
+
+def generate_waypoints_json(*waypoints):
+    data = {"waypoints": [point for point in waypoints], "options": {"travelMode": "car", "vehicleCommercial": False, "waypointConstraints": {"originIndex": 0}}}
+    json_data = json.dumps(data, indent=4)
+    return json_data
+
+def get_route(json_data, api_key=tomtom_api_key):
+    api_call_route = f"http://api.tomtom.com/routing/waypointoptimization/1?key={api_key}"
+    headers = {'Content-Type': 'application/json', 'accept': '*/*'}
+    response = requests.post(api_call_route, data=json_data, headers=headers)
+    data = response.json()
+    return data
+
+
