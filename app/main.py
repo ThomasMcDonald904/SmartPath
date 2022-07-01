59909f470fd985ef96f23e7b1c93331b9116b76d
ThomasMcDonald904 <naynaytom3000@gmail.com>
2022-07-01 17:11:19 -0400

    AWEDFSD

--- app/main.py
+++ app/main.py
@@ -58,7 +58,7 @@ def address_to_lonlat(_address: str, _city: str, api_key=tomtom_api_key):
     api_call_geocode = f"https://api.tomtom.com/search/2/geocode/{url_encoded_address}.json?storeResult=false&countrySet=Canada&view=Unified&key={api_key}"
 
     response = requests.request("GET", api_call_geocode)
-    data = response.text
+    data = response.json
 
     for i in data["results"]:
         if i["type"] == "Point Address" and i["address"]["municipality"] == _city:
@@ -74,7 +74,7 @@ def lon_lat_to_address(lon_lat_dict, api_key=tomtom_api_key):
     lon_lat_pair_encoded = urllib.parse.quote(lon_lat_pair)
     api_call_reverse_geocode = f"https://api.tomtom.com/search/2/reverseGeocode/crossStreet/{lon_lat_pair_encoded}.json?returnSpeedLimit=false&radius=10000&returnRoadUse=false&callback=cb&allowFreeformNewLine=false&returnMatchType=false&view=Unified&key={api_key}"
     response = requests.request("GET", api_call_reverse_geocode)
-    data = response.text
+    data = response.json
     return data
 
 def generate_waypoints_json(*waypoints):
@@ -86,7 +86,7 @@ def get_route(json_data, api_key=tomtom_api_key):
     api_call_route = f"http://api.tomtom.com/routing/waypointoptimization/1?key={api_key}"
     headers = {'Content-Type': 'application/json', 'accept': '*/*'}
     response = requests.post(api_call_route, data=json_data, headers=headers)
-    data = response.text
+    data = response.json
     return data
 
 
